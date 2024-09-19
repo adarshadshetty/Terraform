75b84de1b76f9dfb9a1baa85c9c39d851a5fb9ce
unknown <adarshadshetty18@gmail.com>
2024-09-19 13:01:58 +0530

    Terraform execution file

--- automateAWSInfra-Project/main.tf
+++ automateAWSInfra-Project/main.tf
@@ -7,6 +7,9 @@ variable "subnet_cidr_block" {}
 variable "avail_zone" {}
 variable "env_prefix" {}
 variable "my_ip" {}
+variable "instance_type" {}
+# variable "my_public_key" {}
+variable "public_key_location" {}
 
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block  
@@ -104,11 +107,11 @@ resource "aws_security_group" "myapp-sg" {
 
 
 
-/*
+
 #  use of default SG
 
-resource "aws_default_security_group" "default-myapp-sg" {
-  name = "myapp-sg-default"
+resource "aws_default_security_group" "default-sg" {
+  # name = "myapp-sg-default"
   vpc_id = aws_vpc.myapp-vpc.id
 
   ingress {
@@ -135,7 +138,78 @@ resource "aws_default_security_group" "default-myapp-sg" {
   }
 }
 
+
+# Amazon Machine Image for EC2
+# Set AMI dynamically
+
+data "aws_ami" "latest-amazon-linux-image" {
+  most_recent = true
+  owners = ["amazon"]
+  filter {                  // Adding filter define the criteria ami
+    name = "name"
+    values = ["amzn2-ami-kernel-*-x86_64-gp2"]
+  }
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+output "aws_ami_id" {
+  value = data.aws_ami.latest-amazon-linux-image.id 
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.myapp-server.public_ip
+}
+
+
+# Automated the creation of key-pair here
+resource "aws_key_pair" "ssh-key" {
+  key_name = "newawsVersion"
+  public_key = file(var.public_key_location)
+  // public_key = var.my_public_key
+  // A key pair must already exist locally on your machine
+  // We already generated a key pair for Git repo or DigitalOcean Droplet
+}
+
+
+
+
+resource "aws_instance" "myapp-server" {
+  ami = data.aws_ami.latest-amazon-linux-image.id
+  instance_type = var.instance_type
+  
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+  availability_zone = var.avail_zone
+
+  associate_public_ip_address = true 
+  key_name = aws_key_pair.ssh-key.key_name
+
+# Run entrypoint script to start Docker container
+/*
+user_data = <<EOF
+              #!/bin/bash
+              sudo yum update -y && sudo yum install -y docker
+              sudo systemctl start docker
+              sudo usermod -aG docker ec2-user
+              docker run -p 8080:80 nginx 
+            EOF
 */
 
+// Extract to shell script
+user_data = file("entry-script.sh")
+
+user_data_replace_on_change = true //
+
+  tags = {
+    Name = "${var.env_prefix}-server"
+  }
+}
+
+// vpc id & subnet, sg is optional (It created in default region)
+// mv your pem file to ~/.ssh/ folder , change to chmod 400 to pem file
 
 
+# Run entrypoint script to start Docker container
